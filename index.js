441475d9ce46fb4b1411ed6aa7ef0098c2ff6d45
rashelmahomud <rashelmahmudraj1998@gmail.com>
2022-04-25 23:14:17 +0600

    added new setup

--- index.js
+++ index.js
@@ -51,6 +51,18 @@ async function run() {
         });
 
         // Ordser collections api all here
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = {email};
+            const corsur = orderCollection.find(query); 
+            // shoop golla pate hola aibabe likte hobe.
+            const orders = await corsur.toArray();
+            res.send(orders);
+
+        })
+
+
+
         app.post('/order', async(req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
